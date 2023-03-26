d5405489307d674dcb28090038b6d570010d3c3e
GabsSantos <gabriele.ss@icloud.com>
2023-03-26 18:15:21 -0300

    For

---
+++ exercicio23/src/exercicio23/For.java
@@ -0,0 +1,17 @@
+package exercicio23;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i =1; i <5; i++) {
+			int y = i -1;
+			int x = i * 10;
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
