e07b1b10c692f0f15e6b149191161f42153644cb
GabsSantos <gabriele.ss@icloud.com>
2023-03-30 23:13:20 -0300

    Classes

---
+++ exercicio38/src/entities/Rectangle.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Rectangle {
+	
+	public double width;
+	public double heigth;
+	
+	public double area() {
+		return width * heigth;
+		
+	}
+	
+	public double perimeter() {
+		return 2 * (width + heigth);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt((heigth * heigth) + (width * width));
+	}
+	
+	
+
+}
