7e8e46920e9ca13a6f13031aecf9929990dd7b98
GabsSantos <gabriele.ss@icloud.com>
2023-04-12 11:04:29 -0300

    Manipulando Date com Calendar

---
+++ exercicio75/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		int month = 1 + cal.get(Calendar.MONTH);
+		int minutes = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		
+		/*cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		System.out.println(sdf.format(d));*/
+		System.out.println("month = " + month);
+		System.out.println("minutes = " + minutes);
+		System.out.println("seconds = " + sec);
+		
+
+	}
+
+}
